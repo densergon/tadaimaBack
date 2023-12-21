da752a046c6fb5568ac95d966e1d9b300fc68883
Daniel Serna <deniel.naruto@gmail.com>
2023-12-21 05:43:16 -0600

    sql updated

--- src/models/database.sql
+++ src/models/database.sql
@@ -1,106 +1,169 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `mydb` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mydb`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.35
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+--
+-- Table structure for table `clases`
+--
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+DROP TABLE IF EXISTS `clases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clases` (
+  `idClases` int NOT NULL AUTO_INCREMENT,
+  `asignatura` varchar(45) DEFAULT NULL,
+  `profesor` int DEFAULT NULL,
+  PRIMARY KEY (`idClases`),
+  KEY `profesor_idx` (`profesor`),
+  CONSTRAINT `profesor` FOREIGN KEY (`profesor`) REFERENCES `usuarios` (`idUsuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `mydb`.`Roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Roles` (
-  `idRoles` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `descripcion` VARCHAR(45) NULL,
-  PRIMARY KEY (`idRoles`))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `clases`
+--
 
+LOCK TABLES `clases` WRITE;
+/*!40000 ALTER TABLE `clases` DISABLE KEYS */;
+INSERT INTO `clases` VALUES (1,'Matematicas',3),(2,'Español',6),(3,'Ingenieria de Software',3),(4,'Programacion estructurada',3);
+/*!40000 ALTER TABLE `clases` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `mydb`.`Usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Usuarios` (
-  `idUsuarios` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `apellidoPaterno` VARCHAR(45) NULL,
-  `apellidoMaterno` VARCHAR(45) NULL,
-  `correoElectronico` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  `rol` INT NULL,
-  `boleta` VARCHAR(45) NULL,
-  PRIMARY KEY (`idUsuarios`),
-  INDEX `rol_idx` (`rol` ASC) VISIBLE,
-  CONSTRAINT `rol`
-    FOREIGN KEY (`rol`)
-    REFERENCES `mydb`.`Roles` (`idRoles`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `inscripciones`
+--
 
-
--- -----------------------------------------------------
--- Table `mydb`.`Clases`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Clases` (
-  `idClases` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `profesor` INT NULL,
-  PRIMARY KEY (`idClases`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Inscripciones`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Inscripciones` (
-  `idInscripciones` INT NOT NULL AUTO_INCREMENT,
-  `idAlumno` INT NOT NULL,
-  `idClase` INT NOT NULL,
+DROP TABLE IF EXISTS `inscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inscripciones` (
+  `idInscripciones` int NOT NULL AUTO_INCREMENT,
+  `idAlumno` int NOT NULL,
+  `idClase` int NOT NULL,
   PRIMARY KEY (`idInscripciones`),
-  INDEX `clase_idx` (`idClase` ASC) VISIBLE,
-  INDEX `alumno_idx` (`idAlumno` ASC) VISIBLE,
-  CONSTRAINT `class`
-    FOREIGN KEY (`idClase`)
-    REFERENCES `mydb`.`Clases` (`idClases`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `alumno`
-    FOREIGN KEY (`idAlumno`)
-    REFERENCES `mydb`.`Usuarios` (`idUsuarios`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `alumno_idx` (`idAlumno`),
+  CONSTRAINT `alumno` FOREIGN KEY (`idAlumno`) REFERENCES `usuarios` (`idUsuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `inscripciones`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`Tareas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Tareas` (
-  `idTareas` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `descripcion` VARCHAR(45) NULL,
-  `fechaInicio` DATETIME NULL,
-  `fechaEntrega` DATETIME NULL,
-  `idClase` INT NOT NULL,
-  PRIMARY KEY (`idTareas`),
-  INDEX `clase_idx` (`idClase` ASC) VISIBLE,
-  CONSTRAINT `clase`
-    FOREIGN KEY (`idClase`)
-    REFERENCES `mydb`.`Clases` (`idClases`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+LOCK TABLES `inscripciones` WRITE;
+/*!40000 ALTER TABLE `inscripciones` DISABLE KEYS */;
+INSERT INTO `inscripciones` VALUES (1,4,1);
+/*!40000 ALTER TABLE `inscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `roles`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `idRoles` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idRoles`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrador','El que administra'),(2,'Profesor','El que deja tarea'),(3,'Estudiante','Alumno');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tareas`
+--
+
+DROP TABLE IF EXISTS `tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tareas` (
+  `idTareas` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `fechaInicio` datetime DEFAULT NULL,
+  `fechaEntrega` datetime DEFAULT NULL,
+  `idClase` int NOT NULL,
+  `prioridad` int NOT NULL,
+  PRIMARY KEY (`idTareas`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tareas`
+--
+
+LOCK TABLES `tareas` WRITE;
+/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
+INSERT INTO `tareas` VALUES (1,'Problemario','Hacer el problemario',NULL,NULL,1,0);
+/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUsuarios` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellidoPaterno` varchar(45) DEFAULT NULL,
+  `apellidoMaterno` varchar(45) DEFAULT NULL,
+  `correoElectronico` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `rol` int DEFAULT NULL,
+  `boleta` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUsuarios`),
+  KEY `rol_idx` (`rol`),
+  CONSTRAINT `rol` FOREIGN KEY (`rol`) REFERENCES `roles` (`idRoles`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (2,'Daniel','Serna','Gonzalez','dan@ser.com','Limonada123',1,NULL),(3,'Pedro','Armendariz','Jimenez','pedro@mail.com','Limonada123',2,NULL),(4,'Diego','Hernandez','Lopez','diego@mail.com','Limonada123',3,'2014090687'),(5,'Juan','Guarnizo','Lopez','gu2a@mail.com','Limonada123',3,'2093098876'),(6,'Fanny','Adan','Sosa','fanny@mail.com','Limonada123',2,NULL),(7,'Luis','Jimenez','Lopez','limonada@mail.com','Limonada',2,NULL),(20,'Daniel','Serna','Gonzalez','deded@dwedwedededry.com','123456',2,NULL),(23,'Oscar','Lopez','Jimenez','oscar@mail.com','123456',2,NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-21  5:41:41
