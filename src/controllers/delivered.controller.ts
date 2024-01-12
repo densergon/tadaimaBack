64e914555afe2f29662f881052326c131fd33922
Daniel Serna <deniel.naruto@gmail.com>
2024-01-12 09:05:16 -0600

    calificar

--- src/controllers/delivered.controller.ts
+++ src/controllers/delivered.controller.ts
@@ -15,13 +15,36 @@ export const deliveredController = {
     getDeliveredHomework: async (req: Request, res: Response) => {
         const { id } = req.params;
         try {
-            const result = await pool.query('SELECT * FROM tadaima.entregadas where idEntregadas=?', [id])
+            const result = await pool.query('SELECT * FROM entregadas where idEntregadas=?', [id])
             res.status(200).send(result[0])
+        } catch (error) {
+            console.log(error)
+            res.send({ message: "error" })
+        }
+    },
+    calificar: async (req: Request, res: Response) => {
+        const { id, calificacion } = req.body;
+        try {
+            const result = await pool.query('UPDATE entregadas SET calificacion=? WHERE (idEntregadas=?)', [calificacion, id])
+            res.status(200).send(result[0])
+        } catch (error) {
+            console.log(error)
+            res.send({ message: "error" })
+        }
+    },
+    entregarTarea: async (req: Request, res: Response) => {
+        const { idUsuario, idTarea } = req.body;
+        try {
+            const getIdAlumno = await pool.query(`SELECT idAlumno FROM alumnos where idUsuario=?`, [idUsuario]);
+
+            const idAlumno = ((getIdAlumno[0] as Array<any>)[0] as any).idAlumno;
+            console.log(idAlumno);
+            const result = await pool.execute(`INSERT INTO entregadas (idAlumno,idTarea) VALUES (?,?)`, [idAlumno, idTarea]);
+            //res.status(200).send(result[0])
+            res.send({ message: 'ok' })
         } catch (error) {
             console.log(error)
             res.send({ message: "error" })
         }
     }
 }
-
-const calificar = `UPDATE entregadas SET calificacion=? WHERE (idEntregadas=?)`
--- src/routes/delivered.routes.ts
+++ src/routes/delivered.routes.ts
@@ -3,6 +3,9 @@ import { deliveredController } from '../controllers/delivered.controller'
 
 const router = Router()
 
-router.get('/:id', deliveredController.getDeliveredHomeworks)
+router.get('/all/:id', deliveredController.getDeliveredHomeworks)
+router.get('/one/:id', deliveredController.getDeliveredHomework)
+router.post('/', deliveredController.entregarTarea)
+router.put('/', deliveredController.calificar)
 
 export default router
